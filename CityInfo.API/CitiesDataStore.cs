5003a2997127b8ca447b174b28e599bbc39330d1
Vadym <vadgig@gmail.com>
2018-07-07 19:43:14 -0400

    Added controllers for cities and points of interest. Implemented logging in application output. So far only methods for getting cities and getting, creating, updating and removing points of interests were written.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park",
+                    PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Cental Park",
+                            Description = "The most visited park in US"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A skyscraper located in midtown of Manhattan"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Toronto",
+                    Description = "The one with one of the tallest towers",
+                    PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "CN Tower",
+                            Description = "The highest tower in North America"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "High Park",
+                            Description = "The best park to visit in Toronto"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Montreal",
+                    Description = "The one where everybody speaks French",
+                    PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "The River",
+                            Description = "A well-known river of the city of Montreal"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "The Castle",
+                            Description = "A huge and beatiful castle"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace CityInfo.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .AddMvcOptions(o => o.OutputFormatters.Add(
+                       new XmlDataContractSerializerOutputFormatter()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            loggerFactory.AddDebug();
+
+            //loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+
+            //loggerFactory.AddNLog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();    
+            }
+
+            app.UseStatusCodePages();
+            app.UseMvc();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
